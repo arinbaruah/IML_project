cc60d56c145f5eab009a6ae9813e40ebc346a072
arinbaruah <abar0090@student.monash.edu>
2024-05-16 03:41:40 +1000

    Code finalised in R script

--- rf_tuned.R
+++ rf_tuned.R
@@ -7,15 +7,19 @@ library(tune)
 library(dials)
 library(workflows)
 library(yardstick)
+library(tidyverse)
+
+set.seed(1148)
+water <- read_csv("data/water_train.csv") |> 
+  mutate(status_id = factor(status_id)) 
+water_test <- read_csv("data/water_test.csv") 
 
 
-water <- read_csv("data/water_train.csv")
-water_test <- read_csv("data/water_test.csv")
-
+# Setting tune workflow
 
 tune_spec <- rand_forest(
   mtry = tune(),
-  trees = 500,
+  trees = 1000,
   min_n = tune()
 ) %>%
   set_mode("classification") %>%
@@ -24,16 +28,17 @@ tune_spec <- rand_forest(
 
 
 
-
+# Tune workflow
 
 tune_wf <- workflow() %>%
   add_model(tune_spec) %>%
   add_formula(as.factor(status_id)~.)
 
+# Setting range of hyperparameters for tuning
 
 rf_grid <- grid_regular(
-  mtry(range = c(10, 30)),
-  min_n(range = c(5, 8)),
+  mtry(range = c(5, 30)),
+  min_n(range = c(2, 8)),
   levels = 5
 )
 
@@ -41,10 +46,66 @@ rf_grid
 
 set.seed(1148)
 
-water$report_date <- year(water$report_date)
 rf_folds <- vfold_cv(water[,-1],v = 5) # Use the selection of variables in water dataset
 
 
+
+# Parallel processing enabled for tuning
+
+
+############## !!!!!!! Takes very long time to run  !!!!!!!!! ######################
+
+############## Tuned results are specified below to save time ################
+
+# Random Forest Model Specification (classification)
+# 
+# Main Arguments after tuning :
+#   mtry = 5
+#   trees = 1000
+#   min_n = 2
+
+
+# Can directly set the below specs instead of tuning
+
+
+
+# tune_spec <- rand_forest(
+#   mtry = 5,
+#   trees = 1000,
+#   min_n = 2
+# ) %>%
+#   set_mode("classification") %>%
+#   set_engine("ranger")
+# 
+# 
+# 
+# water$report_date <- year(water$report_date)
+# water_test$report_date <- year(water_test$report_date)
+# 
+# # Choosing the final set of variables and
+# 
+# water <- water %>% select(-c(is_urban))
+# water_test <- water_test %>% select(-c(is_urban))
+# 
+# # Removing junk data
+# 
+# water <- water %>% filter(install_year < 2024)
+# 
+# 
+# 
+# fit_rf <- tune_spec |> 
+#   fit(status_id ~ ., data = water[,-1])
+# 
+# 
+# 
+# 
+# water_test |> 
+#   bind_cols(predict(fit_rf, new_data=water_test, type="prob")) |> 
+#   mutate(pstatus_id = if_else(.pred_n >=0.45,"n","y")) 
+# write_csv(water_ts_pred[,c("ID", "pstatus_id")], file="predictions_new.csv")
+
+
+################################ End ##################################################
 
 
 doParallel::registerDoParallel()
@@ -76,18 +137,40 @@ final_rf <- finalize_model(
 
 final_rf
 
+# Random Forest Model Specification (classification)
+# 
+# Main Arguments after tuning :
+#   mtry = 5
+#   trees = 1000
+#   min_n = 2
+# 
+# Computational engine: ranger 
+
+# Changing report date to be just year
+
+water$report_date <- year(water$report_date)
+water_test$report_date <- year(water_test$report_date)
+
+# Choosing the final set of variables and
+
+water <- water %>% select(-c(is_urban))
+water_test <- water_test %>% select(-c(is_urban))
+
+# Removing junk data
+
+water <- water %>% filter(install_year < 2024)
+
 
 
 rf_fit_tune <- final_rf %>%
   fit(status_id~.,data = water[,-1]) # Use the selection of variables in water dataset
 
 
+# Changing prediction thresholds for "no" as >=0.45
 
-water_test$report_date <- year(water_test$report_date)
-
-water_ts_pred <- water_test |> 
+water_test |> 
   bind_cols(predict(rf_fit_tune, new_data=water_test, type="prob")) |> 
-  mutate(pstatus_id = if_else(.pred_n >=0.5,"n","y")) 
+  mutate(pstatus_id = if_else(.pred_n >=0.45,"n","y")) 
 write_csv(water_ts_pred[,c("ID", "pstatus_id")], file="predictions.csv")
 
 
